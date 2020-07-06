e9a94594ed6ca794fa329f91a749c85427cd72d2
electrocatstudios <phil@electrocatstudios.com>
2020-07-06 12:46:47 +0100

    remvoed spurious lines from example code

--- python_client_example/postScreen.py
+++ python_client_example/postScreen.py
@@ -8,13 +8,10 @@ if __name__ == "__main__":
     channel = grpc.insecure_channel("192.168.0.100:7777")
     stub = screenservice_pb2_grpc.ScreenServerStub(channel)
     payload = screenservice_pb2.ScreenRequest()
-    line1 = screenservice_pb2.Line()
-    line1.line_type = "text"
-    line1.line_value = "another message sent"
+
     payload.line1.line_type = "text" 
     payload.line1.line_value =  "another message sent"
 
-    # timeout = screenservice_pb2.Timeout()
     payload.timeout.length = 10
     payload.timeout.showtimeout = 5
     payload.timeout.returnscreen = "menu"
